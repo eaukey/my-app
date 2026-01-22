180a3d1e54dfb06833c86b4fc2581cd4cf834500
root <romain.fanucci@hotmail.fr>
2026-01-22 17:00:57 +0100

    menu laterale page connexion

--- my-app/app/layout.js
+++ my-app/app/layout.js
@@ -4,7 +4,7 @@ import localFont from "next/font/local";
 import "./globals.css";
 import SideNav from "../components/SideNav";
 import styles from "./Layout.module.css";
-import { AuthProvider } from "../lib/auth";
+import { AuthProvider, useAuth } from "../lib/auth";
 
 // Import des polices
 const geistSans = localFont({
@@ -18,6 +18,20 @@ const geistMono = localFont({
   weight: "100 900",
 });
 
+function Shell({ children }) {
+  const { isAuthenticated } = useAuth();
+  const showSideNav = isAuthenticated;
+
+  return (
+    <div className={styles.shell}>
+      {showSideNav && <SideNav />}
+      <div className={showSideNav ? styles.content : styles.contentNoNav}>
+        {children}
+      </div>
+    </div>
+  );
+}
+
 export default function RootLayout({ children }) {
   return (
     <html lang="fr">
@@ -25,12 +39,7 @@ export default function RootLayout({ children }) {
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <AuthProvider>
-          <div className={styles.shell}>
-            <SideNav />
-            <div className={styles.content}>
-              {children}
-            </div>
-          </div>
+          <Shell>{children}</Shell>
         </AuthProvider>
       </body>
     </html>
